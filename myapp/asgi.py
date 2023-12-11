4eed46db24ecaa77f1792ecd57f2f115955efa35
EC2 Default User <ec2-user@ip-172-31-0-168.eu-west-1.compute.internal>
2023-12-11 19:17:00 +0000

    new changes01

--- myapp/asgi.py
+++ myapp/asgi.py
@@ -1,15 +1,15 @@
 """
 ASGI config for myapp project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
+It exposes the ASGI callable as a module-level variable named 'application'.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
+from django.core.asgi import get_asgi_application  # pylint: disable=import-error, no-name-in-module
 
-from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myapp.settings')
 
---
+++ myapp/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ec2-user/environment/x23109831_devsecops_project/myapp/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
